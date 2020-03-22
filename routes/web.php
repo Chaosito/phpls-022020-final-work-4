60e97610d8e29541348d5df6b323caf06ff1a065
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-22 18:36:50 +0600

    to-do's

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
 | Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+
+/*
+ * To-Do:
+ * Посмотрите наши товары (футер)
+ * Мои заказы
+ * Оформление заказа
+ * Отправка на почту
+ * Профиль?
+ * Редактирование настроек, категорий, продуктов, новостей
+ * Поиск
+ * 
+ *
+ */
 
 Route::get('/', 'HomeController@index')->name('index');
 
