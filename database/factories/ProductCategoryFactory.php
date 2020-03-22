55bcb09b88e10791eb2ceceb13f506092565d370
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-22 22:57:43 +0600

    Added new genres

--- database/factories/ProductCategoryFactory.php
+++ database/factories/ProductCategoryFactory.php
@@ -7,7 +7,8 @@ use Faker\Generator as Faker;
 $factory->define(App\ProductCategories::class, function (Faker $faker) {
     $genres = [
         'Action', 'RPG', 'Квесты', 'Онлайн-игры', 'Стратегии', 'Головоломки',
-        'Extreme', 'Fighting', 'Flight', 'Platform', 'Racing', 'Sports'
+        'Extreme', 'Fighting', 'Flight', 'Platform', 'Racing', 'Sports',
+        'Puzzle', 'Stealth', 'Survival', 'FPS', 'MMO', 'RTS',
     ];
 
     return [
