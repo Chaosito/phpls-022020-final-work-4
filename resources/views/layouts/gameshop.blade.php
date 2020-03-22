819212b2cf8e1170699358eb519fa10d53bea442
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-22 15:43:49 +0600

    stylize paginator

--- resources/views/layouts/gameshop.blade.php
+++ resources/views/layouts/gameshop.blade.php
@@ -12,7 +12,11 @@
 <body>
 <div class="main-wrapper">
     <header class="main-header">
-        <div class="logotype-container"><a href="{{Route('index')}}" class="logotype-link"><img src="{{ asset('img/logo.png') }}" alt="Логотип"></a></div>
+        <div class="logotype-container">
+            <a href="{{Route('index')}}" class="logotype-link">
+                <img src="{{ asset('img/logo.png') }}" alt="Логотип">
+            </a>
+        </div>
         <nav class="main-navigation">
             <ul class="nav-list">
                 <li class="nav-list__item"><a href="#" class="nav-list__item__link">Главная</a></li>
@@ -22,13 +26,20 @@
             </ul>
         </nav>
         <div class="header-contact">
-            <div class="header-contact__phone"><a href="#" class="header-contact__phone-link">Телефон: {{$phone_to}}</a></div>
+            <div class="header-contact__phone">
+                <a href="#" class="header-contact__phone-link">Телефон: {{$phone_to}}</a>
+            </div>
         </div>
         <div class="header-container">
             <div class="payment-container">
                 <div class="payment-basket__status">
-                    <div class="payment-basket__status__icon-block"><a class="payment-basket__status__icon-block__link"><i class="fa fa-shopping-basket"></i></a></div>
-                    <div class="payment-basket__status__basket"><span class="payment-basket__status__basket-value">0</span><span class="payment-basket__status__basket-value-descr">товаров</span></div>
+                    <div class="payment-basket__status__icon-block">
+                        <a class="payment-basket__status__icon-block__link"><i class="fa fa-shopping-basket"></i></a>
+                    </div>
+                    <div class="payment-basket__status__basket">
+                        <span class="payment-basket__status__basket-value">0</span>
+                        <span class="payment-basket__status__basket-value-descr">товаров</span>
+                    </div>
                 </div>
             </div>
 
@@ -78,15 +89,15 @@
                 <div class="sidebar-item__content">
                     <div class="sidebar-news">
                         <div class="sidebar-news__item">
-                            <div class="sidebar-news__item__preview-news"><img src="img/cover/game-2.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+                            <div class="sidebar-news__item__preview-news"><img src="{{ asset('img/cover/game-2.jpg') }}" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
                             <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
                         </div>
                         <div class="sidebar-news__item">
-                            <div class="sidebar-news__item__preview-news"><img src="img/cover/game-1.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+                            <div class="sidebar-news__item__preview-news"><img src="{{ asset('img/cover/game-1.jpg') }}" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
                             <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
                         </div>
                         <div class="sidebar-news__item">
-                            <div class="sidebar-news__item__preview-news"><img src="img/cover/game-4.jpg" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
+                            <div class="sidebar-news__item__preview-news"><img src="{{ asset('img/cover/game-4.jpg') }}" alt="image-new" class="sidebar-new__item__preview-new__image"></div>
                             <div class="sidebar-news__item__title-news"><a href="#" class="sidebar-news__item__title-news__link">О новых играх в режиме VR</a></div>
                         </div>
                     </div>
@@ -97,7 +108,7 @@
             @section('content-top')
                 <div class="content-top">
                     <div class="content-top__text">Купить игры неборого без регистрации смс с торента, получить компкт диск, скачать Steam игры после оплаты</div>
-                    <div class="slider"><img src="img/slider.png" alt="Image" class="image-main"></div>
+                    <div class="slider"><img src="{{ asset('img/slider.png') }}" alt="Image" class="image-main"></div>
                 </div>
             @show
             <div class="content-middle">
@@ -157,7 +168,7 @@
                 <div class="random-product-container__content">
                     <div class="item-product">
                         <div class="item-product__title-product"><a href="#" class="item-product__title-product__link">The Witcher 3: Wild Hunt</a></div>
-                        <div class="item-product__thumbnail"><a href="#" class="item-product__thumbnail__link"><img src="img/cover/game-1.jpg" alt="Preview-image" class="item-product__thumbnail__link__img"></a></div>
+                        <div class="item-product__thumbnail"><a href="#" class="item-product__thumbnail__link"><img src="{{ asset('img/cover/game-1.jpg') }}" alt="Preview-image" class="item-product__thumbnail__link__img"></a></div>
                         <div class="item-product__description">
                             <div class="item-product__description__products-price"><span class="products-price">400 руб</span></div>
                             <div class="item-product__description__btn-block"><a href="#" class="btn btn-blue">Купить</a></div>
@@ -187,6 +198,6 @@
         </div>
     </footer>
 </div>
-<script src="js/main.js"></script>
+<script src="{{ asset('js/main.js') }}"></script>
 </body>
 </html>
